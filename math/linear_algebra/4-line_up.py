5e2e648d84ae7fafdbb56518dae34054f04f1fea
clay <rpctropic13@gmail.com>
2025-01-10 13:40:44 -0600

    adds the elements of 2 arrays

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,14 @@
+#!/ussr/bin/env python3
+"""
+function that adds two arrays element-wise
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    function that returns the sum of two arrays or
+    none if they are not the same size
+    """
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
