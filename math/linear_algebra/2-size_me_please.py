e9aea75ba6cdba0dcc26bc919d5fa4bc54cb5727
clay <rpctropic13@gmail.com>
2025-01-10 13:02:52 -0600

    pycheck

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+"""
+function that calculates the shape of a matrix
+"""
+
 
 def matrix_shape(matrix):
+
+    """
+    takes in a matrix and returns the shape as a list of integers
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
