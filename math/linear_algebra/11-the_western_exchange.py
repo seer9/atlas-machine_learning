6b06f7b1ae711ce2d53d19bf63e683ecb6bc1129
clay <rpctropic13@gmail.com>
2025-01-11 17:13:54 -0600

    initial

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function that transposes a matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+    returns the tuple of the matrix/numpy.ndarray
+    """
+    return matrix.T
