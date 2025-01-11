7d5f2c905385fb79b8fea5bdccc58ed6f3772763
clay <rpctropic13@gmail.com>
2025-01-11 15:05:55 -0600

    concatenates two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concatenates two arrays to one another
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    returns the concatenated array
+    """
+    return arr1 + arr2
