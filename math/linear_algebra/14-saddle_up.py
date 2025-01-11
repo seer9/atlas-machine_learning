8420ac2a62130749ad7772dfa207893aec5d929e
clay <rpctropic13@gmail.com>
2025-01-11 17:38:53 -0600

    initial

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function that performs matrix multiplication
+"""
+
+
+def np_matmul(mat1, mat2):
+    """
+    returns the product of mat1 and mat2
+    """
+    return mat1 @ mat2
