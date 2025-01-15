9be09a4aca91765e2067b34ed5ebe2d093ac91be
clay <rpctropic13@gmail.com>
2025-01-14 18:53:49 -0600

    py/doc checks

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""
+line function
+"""
+
 
 def line():
-
+    """
+    plots a line graph
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
