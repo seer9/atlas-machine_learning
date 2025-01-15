ded6a2047900960b53c566ac56b5930aee1c11cf
clay <rpctropic13@gmail.com>
2025-01-14 18:59:03 -0600

    changed layout of doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
+"""
+in this module the function line is created using matplotlib.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
-"""
-in this module the function line is created using matplotlib
-"""
+
 
 
 def line():
     """
-    creates a line graph
+    creates a line graph.
     """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     """
-    plots the line graph
+    plots the line graph.
     """
     plt.plot(y, 'r')
     plt.xlim(0, 10)
