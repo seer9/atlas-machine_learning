f61892fa6c39fe8ab497c5b281622a5fc9996858
clay <rpctropic13@gmail.com>
2025-01-14 19:01:31 -0600

    py/doc checks

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
+"""
+this module contains the function scatter
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter():
 
+def scatter():
+    """
+    creates a scatter plot.
+    """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
-
+    """
+    plots the scatter plot.
+    """
     plt.title('Men\'s Height vs Weight')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
