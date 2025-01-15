0351bb319a33402c8dfbbce0e6d93e00008e2f3e
clay <rpctropic13@gmail.com>
2025-01-14 19:19:46 -0600

    forgot to add the tick range for x axis

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
+"""
+this module contains the function frequency
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def frequency():
 
+def frequency():
+    """
+    this function creates a histogram.
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
+    """
+    plots the histogram.
+    """
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xticks(range(0, 101, 10))
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
