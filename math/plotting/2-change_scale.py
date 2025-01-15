5ade63edffe99f3510b1aaccb424ba81079cf59c
clay <rpctropic13@gmail.com>
2025-01-14 19:02:45 -0600

    py/doc checks

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
+"""
+this module contains the function change_scale
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def change_scale():
+    """
+    creates a line graph with a logarithmic scale.
+    """
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
     y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
-
+    """
+    plots the line graph.
+    """
     plt.plot(x, y)
     plt.yscale('log')
     plt.title('Exponential Decay of C-14')
