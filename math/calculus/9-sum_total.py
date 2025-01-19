b53d49eaec81d8f99212521a0d1bc80cc948e737
clay <rpctropic13@gmail.com>
2025-01-19 17:47:17 -0600

    square root of n

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module for calculating the sum of the squares of natural numbers
+"""
+
+
+def summation_i_squared(n):
+
+    """
+    takes n as argument and returns the sum of the square roots of natural numbers
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
