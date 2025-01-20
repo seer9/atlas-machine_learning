2778ed27becaf7e28656e33a1705f8757a0d8e59
clay <rpctropic13@gmail.com>
2025-01-20 15:46:17 -0600

    9/y

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+module for the function poly_integral(poly)
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    takes in a  poly and returns the integral of poly
+    """
+    if not isinstance(poly, list) or len(poly) == 0:
+        """if poly is not a list or is empty"""
+        return None
+    if not isinstance(C, (int, float)):
+        """if C is not a float"""
+        return None
+    if len(poly) == 1:
+        """if poly has only one element"""
+        return [C]
+    """return the integral of the polynomial"""
+    return [C] + [poly[i] / (i + 1) for i in range(len(poly))]
