4e04ac29ebf2a226d66b2ec50a7bbd2aa7322579
clay <rpctropic13@gmail.com>
2025-01-22 18:52:12 -0600

    methods z-score and x-value

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -29,3 +29,15 @@ class Normal:
             """
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        calculates the z-score of a given x-value
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        calculates the x-value of a given z-score
+        """
+        return (z * self.stddev) + self.mean
