5b730e37754a49d8c1336cc554b619df571558b2
clay <rpctropic13@gmail.com>
2025-02-06 11:24:51 -0600

    2 for loops

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -10,7 +10,7 @@ class DeepNeuralNetwork:
         """constructor method"""
         if not isinstance(nx, int) or nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0 or not all(isinstance(x, int) and x > 0 for x in layers):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
@@ -21,11 +21,12 @@ class DeepNeuralNetwork:
             size = layers[i - 1]
             prev = nx if i == 1 else layers[i - 2]
 
-            self.__weights[f'W{i}'] = np.random.randn(size, prev) * np.sqrt(2 / prev)
+            self.__weights[f'W{i}'] = (
+                np.random.randn(size, prev) * np.sqrt(2 / prev))
             self.__weights[f'b{i}'] = np.zeros((size, 1))
 
     def forward_prop(self, X):
-        """forward propagation of the network"""
+        """Forward propagation of the network"""
         self.__cache['A0'] = X
 
         for i in range(1, self.L + 1):
@@ -40,15 +41,15 @@ class DeepNeuralNetwork:
 
     @property
     def L(self):
-        """Getter for L"""
+        """Getter method L"""
         return self.__L
 
     @property
     def cache(self):
-        """Getter for cache"""
+        """Getter method cache"""
         return self.__cache
 
     @property
     def weights(self):
-        """Getter for weights"""
+        """Getter method weights"""
         return self.__weights
