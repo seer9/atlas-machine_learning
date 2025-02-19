317c4ea164a58d9bca9ce6bfc395471780642d91
clay <rpctropic13@gmail.com>
2025-02-19 01:14:41 -0600

    pycode check

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """RMSProp optimization algorithm"""
-import tensorflow as tf
+import numpy as np
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """
     updates a variable using the RMSProp optimization algorithm
     """
-    s = beta2 * s + (1 - beta2) * grad**2
-    var = var - alpha * grad / (tf.sqrt(s) + epsilon)
+    new_s = beta2 * s + (1 - beta2) * np.power(grad, 2)
+    new_var = var - alpha * grad / (np.sqrt(new_s) + epsilon)
 
-    return var, s
+    return new_var, new_s
