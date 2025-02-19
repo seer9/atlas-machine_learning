bd59e3f9c207b39bb2a8047c5cb814fffbdc69c3
clay <rpctropic13@gmail.com>
2025-02-19 00:32:50 -0600

    momentum optimization algorithm

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -4,8 +4,8 @@
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """
-    updates a variable using the gradient descent with momentum optimization
-    algorithm
+    updates a variable using the gradient descent
+    with momentum optimization algorithm
     """
     v = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v
