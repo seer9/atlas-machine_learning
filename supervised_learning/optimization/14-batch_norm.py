79d6dd804093f1c92d39fe552d6398004e5cb0d4
clay <rpctropic13@gmail.com>
2025-02-19 02:07:06 -0600

    pycode check

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -9,21 +9,22 @@ def create_batch_norm_layer(prev, n, activation):
     """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.keras.layers.Dense(units=n, kernel_initializer=init)
-    
+
     Z = layer(prev)
-    
+
     gamma = tf.Variable(initial_value=tf.ones((1, n)), name='gamma')
     beta = tf.Variable(initial_value=tf.zeros((1, n)), name='beta')
-    
+
     epsilon = 1e-7
-    
+
     mean, variance = tf.nn.moments(Z, axes=0)
-    
+
     Z_norm = tf.nn.batch_normalization(
         x=Z,
         mean=mean,
         variance=variance,
         offset=beta,
-        scale=gamma, 
+        scale=gamma,
         variance_epsilon=epsilon)
+
     return activation(Z_norm)
