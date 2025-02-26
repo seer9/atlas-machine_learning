6159c90390893900366f11407dbb10d69a103a6c
clay <rpctropic13@gmail.com>
2025-02-26 16:46:38 -0600

    dropout forward prop

---
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""dropout forward prop"""
+import numpy as np
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """
+    conducts forward propagation using Dropout
+    :param X: np.ndarray (nx, m) of input data
+    :param weights: dictionary of weights and biases of the neural network
+    :param L: number of layers in the network
+    :param keep_prob: probability that a node will be kept
+    :return: dictionary containing the output of each layer and the dropout mask used on each layer
+    """
+    cache = {}
+    cache['A0'] = X
+
+    for i in range(L):
+        W = weights['W' + str(i + 1)]
+        b = weights['b' + str(i + 1)]
+        A = cache['A' + str(i)]
+        Z = np.matmul(W, A) + b
+        if i == L - 1:
+            t = np.exp(Z)
+            cache['A' + str(i + 1)] = t / np.sum(t, axis=0, keepdims=True)
+        else:
+            A = np.tanh(Z)
+            D = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
+            A = np.multiply(A, D)
+            A /= keep_prob
+            cache['D' + str(i + 1)] = D
+            cache['A' + str(i + 1)] = A
+    return (cache)
