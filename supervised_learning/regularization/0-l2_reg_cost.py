446b2abf937cfb4ba97f41a003785b94d2e72581
clay <rpctropic13@gmail.com>
2025-02-26 15:21:45 -0600

    L2 regularization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -9,16 +9,16 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
         cost: cost without L2 regularization
         lambtha: regularization parameter
         weights: dictionary of the weights and biases
-        L: number of layers 
+        L: number of layers
         m: data points used
     Returns: cost of L2 regularization
     """
     l2_reg = 0
-    
+
     for i in range(1, L + 1):
         W = weights['W' + str(i)]
         l2_reg += np.sum(np.square(W))
 
     l2_reg *= (lambtha / (2 * m))
-    
+
     return cost + l2_reg
