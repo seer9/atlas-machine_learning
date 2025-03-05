cd6024fced91564bc99f6fc10200e0601802dccf
clay <rpctropic13@gmail.com>
2025-03-05 04:51:37 -0600

    valid convolution

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
-"""performs a valid convolution on grayscale image"""
+"""valid convolution on grayscale image"""
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    """performs a valid convolution on grayscale image"""
+    """
+    performs a valid convolution on grayscale image.
+    Args:
+        images: multiple grayscale images
+        shape (m, h, w) where
+        - m: number of images
+        - h: height in pixels of the images
+        - w: width in pixels of the images
+        kernel: kernel for the convolution
+        shape (kh, kw) where
+        - kh: height of the kernel
+        - kw: width of the kernel
+    """
     m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw = kernel.shape[0], kernel.shape[1]
     nh = h - kh + 1
