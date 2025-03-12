72afbab4e00a7a129c17794d70c744f59c5be00a
clay <rpctropic13@gmail.com>
2025-03-12 01:09:16 -0600

    having weird outputs

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -10,7 +10,7 @@ def lenet5(X):
     conv1 = K.layers.Conv2D(
         filters=6, kernel_size=5,
         padding='same', activation='relu',
-        kernel_initializer=K.initializers.he_normal(seed=None))(X)
+        kernel_initializer=K.initializers.HeNormal())(X)
 
     pool1 = K.layers.MaxPooling2D(
         pool_size=(2, 2), strides=(2, 2))(conv1)
@@ -18,18 +18,18 @@ def lenet5(X):
     conv2 = K.layers.Conv2D(
         filters=16, kernel_size=5,
         padding='valid', activation='relu',
-        kernel_initializer=K.initializers.he_normal(seed=None))(pool1)
+        kernel_initializer=K.initializers.HeNormal())(pool1)
 
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
 
     flatten = K.layers.Flatten()(pool2)
 
     fc1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer=K.initializers.he_normal(seed=None))(flatten)
+                         kernel_initializer=K.initializers.HeNormal())(flatten)
     fc2 = K.layers.Dense(units=84, activation='relu',
-                         kernel_initializer=K.initializers.he_normal(seed=None))(fc1)
+                         kernel_initializer=K.initializers.HeNormal())(fc1)
     fc3 = K.layers.Dense(units=10,
-                         kernel_initializer=K.initializers.he_normal(seed=None))(fc2)
+                         kernel_initializer=K.initializers.HeNormal())(fc2)
 
     model = K.models.Model(X, fc3)
     model.compile(optimizer=K.optimizers.Adam(),
