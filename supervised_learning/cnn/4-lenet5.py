7907ff4378b6755623a779c2c5916093437bac1e
clay <rpctropic13@gmail.com>
2025-03-12 00:46:46 -0600

    changed initializer

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -10,23 +10,23 @@ def lenet5(x, y):
         x: the input images for the network
         y: one-hot encoded labels of x
     returns:
-        y_pred: the networks prediction
+        fc3: the networks prediction
         train_op: networks training operation
         loss: the loss
         accuracy: networks accuracy
     """
-    init = tf.contrib.layers.variance_scaling_initializer()
+    init = tf.keras.initializers.VarianceScaling(scale=2.0)
     act_fuct = tf.nn.relu
 
-    conv1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+    conv1 = tf.layers.Conv2D(filters=6, kernel_size=5,
                              padding='same', activation=act_fuct,
                              kernel_initializer=init)(x)
     pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
-    conv2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+    conv2 = tf.layers.Conv2D(filters=16, kernel_size=5,
                              padding='valid', activation=act_fuct,
                              kernel_initializer=init)(pool1)
-    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(conv2)
 
     flatten = tf.layers.Flatten()(pool2)
 
@@ -34,13 +34,13 @@ def lenet5(x, y):
                           kernel_initializer=init)(flatten)
     fc2 = tf.layers.Dense(units=84, activation=act_fuct,
                           kernel_initializer=init)(fc1)
-    y_pred = tf.layers.Dense(units=10,
+    fc3 = tf.layers.Dense(units=10,
                              kernel_initializer=init)(fc2)
 
-    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    loss = tf.losses.softmax_cross_entropy(y, fc3)
     adam_op = tf.train.AdamOptimizer().minimize(loss)
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(fc3, 1))
     acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    softmax = tf.nn.softmax(y_pred)
+    softmax = tf.nn.softmax(fc3)
 
     return softmax, adam_op, loss, acc
