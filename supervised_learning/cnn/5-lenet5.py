f0d533ad9d0bdf73dbb13e476d0b865fa9007ab2
clay <rpctropic13@gmail.com>
2025-03-12 01:02:57 -0600

    refactoring initializer

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -7,12 +7,10 @@ def lenet5(X):
     """
     function that builds a version of the LeNet-5 architecture
     """
-    init = K.initializers.he_normal(seed=None)
-
     conv1 = K.layers.Conv2D(
         filters=6, kernel_size=5,
         padding='same', activation='relu',
-        kernel_initializer=init)(X)
+        kernel_initializer=K.initializers.he_normal(seed=None))(X)
 
     pool1 = K.layers.MaxPooling2D(
         pool_size=(2, 2), strides=(2, 2))(conv1)
@@ -20,18 +18,18 @@ def lenet5(X):
     conv2 = K.layers.Conv2D(
         filters=16, kernel_size=5,
         padding='valid', activation='relu',
-        kernel_initializer=init)(pool1)
+        kernel_initializer=K.initializers.he_normal(seed=None))(pool1)
 
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
 
     flatten = K.layers.Flatten()(pool2)
 
     fc1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer=init)(flatten)
+                         kernel_initializer=K.initializers.he_normal(seed=None))(flatten)
     fc2 = K.layers.Dense(units=84, activation='relu',
-                         kernel_initializer=init)(fc1)
+                         kernel_initializer=K.initializers.he_normal(seed=None))(fc1)
     fc3 = K.layers.Dense(units=10,
-                         kernel_initializer=init)(fc2)
+                         kernel_initializer=K.initializers.he_normal(seed=None))(fc2)
 
     model = K.models.Model(X, fc3)
     model.compile(optimizer=K.optimizers.Adam(),
