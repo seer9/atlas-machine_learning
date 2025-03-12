f48306faf45b5ee7c312558e67c7983c3d80fb26
clay <rpctropic13@gmail.com>
2025-03-12 00:31:15 -0600

    pycode check for indentions

---
+++ supervised_learning/cnn/4-lenet5.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""builds a modified version of the LeNet-5 architecture using tensorflow"""
+import tensorflow as tf
+
+
+def lenet5(x, y):
+    """
+    function that builds a version of the LeNet-5 architecture
+    args:
+        x: the input images for the network
+        y: one-hot encoded labels of x
+    returns:
+        y_pred: the networks prediction
+        train_op: networks training operation
+        loss: the loss
+        accuracy: networks accuracy
+    """
+    init = tf.contrib.layers.variance_scaling_initializer()
+    act_fuct = tf.nn.relu
+
+    conv1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+                             padding='same', activation=act_fuct,
+                             kernel_initializer=init)(x)
+    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+
+    conv2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                             padding='valid', activation=act_fuct,
+                             kernel_initializer=init)(pool1)
+    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+
+    flatten = tf.layers.Flatten()(pool2)
+
+    fc1 = tf.layers.Dense(units=120, activation=act_fuct,
+                          kernel_initializer=init)(flatten)
+    fc2 = tf.layers.Dense(units=84, activation=act_fuct,
+                          kernel_initializer=init)(fc1)
+    y_pred = tf.layers.Dense(units=10,
+                             kernel_initializer=init)(fc2)
+
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    adam_op = tf.train.AdamOptimizer().minimize(loss)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    return y_pred, adam_op, loss, acc
