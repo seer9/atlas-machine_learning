7dc4918fc33b702eec3ce4a94bb453344cf27e75
clay <rpctropic13@gmail.com>
2025-03-18 07:34:18 -0500

    identity block

---
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""function that builds an identity block"""
+from tensorflow import keras as K
+
+
+def identity_block(A_prev, filters):
+    """
+    Builds an identity block as described.
+
+    Args:
+        A_prev: output from the previous layer
+        filters: tuple or list containing F11, F3, F12.
+
+    Returns:
+        The activated output of the identity block.
+    """
+    [F11, F3, F12] = filters
+
+    # He normal initializer with seed set to 0
+    he_init = K.initializers.HeNormal(seed=0)
+
+    # First 1x1 convolution
+    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), padding='same',
+                        kernel_initializer=he_init)(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # 3x3 convolution
+    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
+                        kernel_initializer=he_init)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # Second 1x1 convolution
+    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same',
+                        kernel_initializer=he_init)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+
+    # Add shortcut (identity connection)
+    X = K.layers.Add()([X, A_prev])
+    X = K.layers.Activation('relu')(X)
+
+    return X
