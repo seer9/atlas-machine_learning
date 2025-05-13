1524621767b1790fcf17e882e830b56085e5a83c
clay <rpctropic13@gmail.com>
2025-05-13 17:50:04 -0500

    cofactor matrix

---
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""derterminant of a matrix"""
+
+
+def determinant(matrix):
+    """
+    Calculates the determinant of a matrix.
+
+    Args:
+        matrix: A square matrix.
+
+    Returns:
+        The determinant of the matrix.
+    """
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        if matrix == [[]]:
+            return 1
+        raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    for c in range(len(matrix)):
+        det += ((-1) ** c) * matrix[0][c] * determinant(
+            [row[:c] + row[c + 1:] for row in matrix[1:]])
+
+    return det
+
+
+def cofactor(matrix):
+    """
+    Calculates the cofactor matrix of a matrix.
+
+    Args:
+        matrix: A square matrix.
+
+    Returns:
+        The cofactor matrix of the input matrix.
+    """
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    for i in matrix:
+        if len(matrix) != len(i):
+            raise ValueError("matrix must be a non-empty square matrix")
+    if len(matrix) == 1:
+        return [[1]]
+    cofactors = []
+    for i in range(len(matrix)):
+        cofactors.append([])
+        for j in range(len(matrix)):
+            minor_matrix = [row[:j] + row[j + 1:]
+                            for row in matrix[:i] + matrix[i + 1:]]
+            cofactors[i].append(((-1) ** (i + j)) * determinant(minor_matrix))
+    return cofactors
