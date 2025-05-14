04a9e3c2b7fb671589c0c1b86f8b8fc56c3e8201
clay <rpctropic13@gmail.com>
2025-05-13 20:17:14 -0500

    changed value error raise

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -16,7 +16,7 @@ def correlation(C):
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
-        raise ValueError("C must be a non-empty square matrix")
+        raise ValueError("C must be a 2D square matrix")
 
     v = np.diag(C)
     sd = np.sqrt(v)
