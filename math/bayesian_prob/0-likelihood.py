978b098729843b3910a520a2c2bde97be6803883
clay <rpctropic13@gmail.com>
2025-05-13 22:27:24 -0500

    pycode check

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,7 +13,7 @@ def likelihood(X, n, P):
         P: A numpy.ndarray of shape (d, d) representing the covariance matrix.
 
     Returns:
-        A numpy.ndarray of shape (n, 1) containing the likelihood of each data point.
+        A numpy.ndarray containing the likelihood of each data point.
     """
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
@@ -25,10 +25,10 @@ def likelihood(X, n, P):
 
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    
+
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
-    
+
     patience = np.math.factorial(n)
     ill = np.math.factorial(X) * np.math.factorial(n - X)
     coef = patience / ill
