543f4536474e66a53f1d5b6816ff6bed519950e0
clay <rpctropic13@gmail.com>
2025-05-13 20:24:25 -0500

    changed type error raise

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -8,7 +8,7 @@ class MultiNormal:
     def __init__(self, data):
         """initializes the distribution"""
         if type(data) is not np.ndarray or len(data.shape) != 2:
-            raise TypeError("data must be a numpy.ndarray")
+            raise TypeError("data must be a 2D numpy.ndarray")
         if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1).reshape(-1, 1)
