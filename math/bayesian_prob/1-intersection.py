883b8880546b1b8af8b60fc8f56f29444f0f76f6
clay <rpctropic13@gmail.com>
2025-05-13 22:37:52 -0500

    calulating hypothetical probabilities

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""likelihood matrix"""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """
+    calculates the intersection of obtaining this data with the various hypothetical probabilities:
+
+    Args:
+        X: A numpy.ndarray of shape (n, d) containing the data points.
+        n: The number of data points.
+        P: A numpy.ndarray of shape (d, d) representing the covariance matrix.
+        Pr: A numpy.ndarray of shape (d, d) containing the prior beliefs of P
+
+    Returns:
+        A numpy.ndarray containing the intersection of obtaining x and n 
+        with each probability in P, respectively
+    """
+    factorial = np.math.factorial
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        str = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(str)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("Pr must be a numpy.ndarray with the same shape as P")
+    if not np.isclose(np.sum(Pr), 1.0):
+        raise ValueError("Pr must sum to 1")
+    
+    likelihood = []
+    intersection = []
+    coef = factorial(n) / (factorial(x) * factorial(n - x))
+    for i in range(len(P)):
+        likelihood.append(coef * (P[i] ** x) * ((1 - P[i]) ** (n - x)))
+        intersection.append(likelihood[i] * Pr[i])
+    return np.array(intersection)
