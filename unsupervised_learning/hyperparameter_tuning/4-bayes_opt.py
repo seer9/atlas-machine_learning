51054052d3cd0a83692476d69e362700e1ab95c3
clay <rpctropic13@gmail.com>
2025-06-09 14:32:20 -0500

    Computing the acquisition

---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Bayesian Optimization with Gaussian Process"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """
+    Class for Bayesian Optimization using Gaussian Process.
+
+    Attributes:
+        gp: An instance of GaussianProcess.
+        X_s: A numpy.ndarray of shape (m, d) representing the points to sample.
+    """
+    def __init__(self, f, X_init, Y_init, bounds,
+                 ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """
+        Bayesian Optimization to optimize, initial data, and parameters.
+
+        Args:
+            f: The black-box function to be optimized.
+            X_init: representing the initial input data points.
+            Y_init: representing the initial target values.
+            bounds: representing the bounds of the space to search.
+            ac_samples: samples to consider for the acquisition function.
+            l: The length-scale parameter for the Gaussian Process kernel.
+            sigma_f: The signal variance for the Gaussian Process kernel.
+            xsi: trade-off parameter for the acquisition function.
+            minimize: indicator whether to minimize or maximize the objective.
+        """
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        X_s = np.linspace(bounds[0], bounds[1], ac_samples)
+        self.X_s = X_s.reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """
+        Computes the acquisition function based on the Gaussian Process.
+
+        Returns:
+            X_next: representing the next best sample point.
+            EI: containing the expected improvement.
+        """
+        mu, sigma = self.gp.predict(self.X_s)
+        if self.minimize:
+            mu = -mu
+
+        with np.errstate(divide='ignore'):
+            Z = (mu - np.max(mu) + self.xsi) / sigma
+            EI = (
+                mu - np.max(mu) + self.xsi) * norm.cdf(Z) + sigma * norm.pdf(Z)
+
+        EI[sigma == 0.0] = 0.0
+
+        # Find the index of the maximum EI
+        X_next = self.X_s[np.argmax(EI)].reshape(1,)
+
+        return X_next, EI
