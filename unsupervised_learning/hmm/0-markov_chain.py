278963531659ffe7232e5519d42543ce360089c9
clay <rpctropic13@gmail.com>
2025-06-12 09:31:06 -0500

    pycodestyle

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -5,15 +5,15 @@ import numpy as np
 
 def markov_chain(P, s, t=1):
     """
-    Computes the state of a Markov chain after t iterations.
+    computes the state of a Markov chain after t iterations.
 
     Args:
-        P: A numpy.ndarray of shape (n, n) representing the transition matrix.
-        s: A numpy.ndarray of shape (n,) representing the initial state.
+        P: represents the transition matrix.
+        s: represents the initial state.
         t: The number of iterations to perform.
 
     Returns:
-        A numpy.ndarray of shape (n,) representing the state after t iterations.
+        the state after t iterations.
     """
     current = s
     for _ in range(t):
