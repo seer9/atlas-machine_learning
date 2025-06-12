22727979c5731a72494863989eba41bc31ff1feb
clay <rpctropic13@gmail.com>
2025-06-12 11:42:53 -0500

    pycode check

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -12,26 +12,24 @@ def regular(P):
             n is the number of states in the markov chain
     Returns: a numpy.ndarray of shape (1, n) containing the steady state probabilities, or None on failure
     """
-    if not isinstance(P, np.ndarray):
-        return None
-    if P.shape[0] != P.shape[1]:
-        return None
-    if len(P.shape) != 2:
-        return None
-    if not np.allclose(P.sum(axis=1), 1):
-        return None
-
-    n = P.shape[0]
-    I = np.eye(n)
-    A = np.subtract(I, P)
-    A[-1, :] = 1
-    b = np.zeros(n)
-    b[-1] = 1
     try:
-        steady_state = np.linalg.solve(A, b)
-    except np.linalg.LinAlgError:
-        return None
+        if not isinstance(P, np.ndarray):
+            return None
+        if P.shape[0] != P.shape[1]:
+            return None
+        if len(P.shape) != 2:
+            return None
+        if not np.allclose(P.sum(axis=1), 1):
+            return None
 
-    if np.any(steady_state < 0):
+        n = P.shape[0]
+        evals, evecs = np.linalg.eig(P.T)
+        # normalize
+        state = (evecs / evecs.sum())
+
+        new_S  = np.dot(state.T, P)
+        for i in new_S:
+            if (i >= 0).all() and np.isclose(i.sum(), 1):
+                return i.reshape(1, n)
+    except Exception:
         return None
-    return steady_state.reshape(1, n)
