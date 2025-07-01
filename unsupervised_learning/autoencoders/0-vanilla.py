3f2e3b534914fca02883957bc0ae3c0b66226308
clay <rpctropic13@gmail.com>
2025-07-01 06:19:43 -0500

    pycode

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -6,9 +6,9 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     """Creates a simple autoencoder with the specified dimensions.
 
     Args:
-        input_dims: The dimensionality of the input data.
-        hidden_layers: List of integers specifying the number of units in each hidden layer.
-        latent_dims: The dimensionality of the latent space.
+        input_dims: input data
+        hidden_layers: number of units in each hidden layer
+        latent_dims: the latent space
 
     Returns:
         tuple: Encoder, decoder, and autoencoder models.
@@ -18,18 +18,19 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     for units in hidden_layers:
         x = keras.layers.Dense(units, activation='relu')(x)
     latent = keras.layers.Dense(latent_dims, activation='relu')(x)
-    
+
     encoder = keras.Model(inputs, latent)
-    
+
     x = latent
     for units in reversed(hidden_layers):
         x = keras.layers.Dense(units, activation='relu')(x)
     outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
-    
+
     decoder = keras.Model(latent, outputs, name='decoder')
-    
-    autoencoder = keras.Model(inputs, decoder(encoder(inputs)), name='autoencoder')
-    
-    autoencoder.compile(optimizer='adam')
-    
+
+    autoencoder = keras.Model(
+        inputs, decoder(encoder(inputs)), name='autoencoder')
+
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
     return encoder, decoder, autoencoder
