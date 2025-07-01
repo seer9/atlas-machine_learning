4ed1e4079f057dd85c403e0c6b444e0e2b2ad68c
clay <rpctropic13@gmail.com>
2025-07-01 07:06:28 -0500

    different apprpoach

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -18,38 +18,37 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     inputs = keras.Input(shape=(input_dims,))
     x = inputs
 
-    # Hidden layers
+    # Hidden layers with relu activation
     for units in hidden_layers:
         x = keras.layers.Dense(units, activation='relu')(x)
 
     # Latent space parameters
-    z_mean = keras.layers.Dense(latent_dims)(x)
-    z_log_var = keras.layers.Dense(latent_dims)(x)
+    z_mean = keras.layers.Dense(latent_dims, activation=None)(x)  # Mean of the latent space
+    z_log_var = keras.layers.Dense(latent_dims, activation=None)(x)  # Log variance of the latent space
 
     # Sampling layer
     def sampling(args):
         z_mean, z_log_var = args
-        epsilon = keras.backend.random_normal(
-            shape=keras.backend.shape(z_mean))
+        epsilon = keras.backend.random_normal(shape=keras.backend.shape(z_mean))
         return z_mean + keras.backend.exp(z_log_var / 2) * epsilon
 
     # Latent representation
     latent = keras.layers.Lambda(sampling)([z_mean, z_log_var])
 
     # Encoder model
-    encoder = keras.Model(inputs, [z_mean, z_log_var, latent], name='encoder')
+    encoder = keras.Model(inputs, [latent, z_mean, z_log_var], name='encoder')
 
     # Decoder model
-    x = latent
+    latent_inputs = keras.Input(shape=(latent_dims,))
+    x = latent_inputs
     for units in reversed(hidden_layers):
         x = keras.layers.Dense(units, activation='relu')(x)
     outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
-    decoder = keras.Model(latent, outputs, name='decoder')
+    decoder = keras.Model(latent_inputs, outputs, name='decoder')
 
     # Autoencoder model
-    autoencoder = keras.Model(
-        inputs, decoder(encoder(inputs)), name='autoencoder')
+    autoencoder = keras.Model(inputs, decoder(encoder(inputs)[0]), name='autoencoder')
 
     # Compile the autoencoder
     autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
