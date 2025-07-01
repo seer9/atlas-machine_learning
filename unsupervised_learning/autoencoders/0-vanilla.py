3b8b727689bb190a81c59a85d626372103ab9a65
clay <rpctropic13@gmail.com>
2025-07-01 06:13:20 -0500

    basic autoencoder

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates a simple autoencoder with the specified dimensions.
+
+    Args:
+        input_dims: The dimensionality of the input data.
+        hidden_layers: List of integers specifying the number of units in each hidden layer.
+        latent_dims: The dimensionality of the latent space.
+
+    Returns:
+        tuple: Encoder, decoder, and autoencoder models.
+    """
+    inputs = keras.Input(shape=(input_dims,))
+    x = inputs
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation='relu')(x)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(x)
+    
+    encoder = keras.Model(inputs, latent)
+    
+    x = latent
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation='relu')(x)
+    outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    
+    decoder = keras.Model(latent, outputs, name='decoder')
+    
+    autoencoder = keras.Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+    
+    autoencoder.compile(optimizer='adam', loss='mse')
+    
+    return encoder, decoder, autoencoder
