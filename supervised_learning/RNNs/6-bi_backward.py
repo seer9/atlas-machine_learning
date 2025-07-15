a53baa3ab3e2d602c2ea4da29bea8de4b2507675
clay <rpctropic13@gmail.com>
2025-07-15 15:20:33 -0500

    rnn bidirectional cell backward

---
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Bidirectional RNN Cell"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """Bidirectional class"""
+    def __init__(self, i, h, o):
+        """Initialize the bidirectional cell
+
+        Args:
+            i: the data
+            h: the hidden state
+            o: the outputs
+        """
+        self.Whf = np.random.randn(i + h, h)
+        self.Whb = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(2 * h, o)
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Perform forward propagation
+
+        Args:
+            h_prev: the previous hidden state
+            x_t: the data input for the cell
+
+        Returns:
+            h_next: the next hidden state
+        """
+        prev_x = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(prev_x, self.Whf) + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """Perform backward propagation
+
+        Args:
+            h_next: the next hidden state
+            x_t: the data input for the cell
+
+        Returns:
+            h_prev: the previous hidden state
+        """
+        next_x = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.dot(next_x, self.Whb) + self.bhb)
+        return h_prev
