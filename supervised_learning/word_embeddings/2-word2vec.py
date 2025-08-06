1e29da56fbc56918b068f6e2cfea66f9bde520d3
clay <rpctropic13@gmail.com>
2025-08-05 23:17:53 -0500

    train the model

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -33,4 +33,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
                      epochs=epochs,
                      seed=seed,
                      workers=workers)
+    model.train(sentences,
+                total_examples=model.corpus_count,
+                epochs=model.epochs)
     return model
