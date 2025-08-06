10b9b69dab34fcd8fd399f10147f5e221b799ad0
clay <rpctropic13@gmail.com>
2025-08-05 23:10:19 -0500

    fixed model value

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """word2vec model"""
-from gensim.models import Word2Vec
-
+import gensim
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Create a word2vec model
@@ -25,7 +24,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
     else:
         cbow_flag = 1
 
-    model = Word2Vec(sentences=sentences,
+    model = gensim.models.Word2Vec(sentences=sentences,
                      vector_size=vector_size,
                      min_count=min_count,
                      window=window,
