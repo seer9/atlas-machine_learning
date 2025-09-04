9a2767e5d0bd7b512b5c08818f77f552071f2bf0
clay <rpctropic13@gmail.com>
2025-09-04 10:15:13 -0500

    frostlake env

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""using the frostlake environment in gym"""
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """loads the frostlake environment from gym
+
+    Args:
+        desc: A list. A custom description of the map to load.
+            Defaults to None.
+        map_name: A str. The name of the map to load.
+            Defaults to None.
+        is_slippery: Whether to make the surface slippery.
+            Defaults to False.
+
+    Returns:
+        gym.Env: The loaded frostlake environment.
+    """
+    return gym.make('FrozenLake-v1', desc=desc,
+                    map_name=map_name, is_slippery=is_slippery)
