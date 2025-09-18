1d65e489dabe4738f6f74802550943dbf4ba3736
clay <rpctropic13@gmail.com>
2025-09-18 10:55:46 -0500

    computing policy gradient

---
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def policy(matrix, weight):
+    """computes the policy with a weight of a matrix.
+    Args:
+        matrix: the number of examples, n is the number of features.
+        weight: where k is the number of classes.
+    Returns: the softmax probabilities for each class, respectively.
+    """
+    z = matrix @ weight
+    # compute softmax
+    exp = np.exp(z - np.max(z))
+    return exp / np.sum(exp)
