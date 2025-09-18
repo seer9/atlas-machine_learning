c45085d978dcf193c34e4f1b375fad94d2e7def4
clay <rpctropic13@gmail.com>
2025-09-18 11:36:10 -0500

    rershaped state to 2d array

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -12,5 +12,24 @@ def policy(matrix, weight):
     """
     z = matrix @ weight
     # compute softmax
-    exp = np.exp(z - np.max(z))
-    return exp / np.sum(exp)
+    exp = np.exp(z)
+    softmax = exp / np.sum(exp)
+    return softmax
+
+
+def policy_gradient(state, weight):
+    """compute the gradient of the policy function with respect to its weights.
+    Args:
+        state: matrix representing the current observation of the environment.
+        weight: matrix of random weights.
+    Return: the action and the gradient (in this order).
+    """
+    # Reshape state to ensure it's a 2D array
+    s = state.reshape(1, -1)
+    # pass to policy
+    pol = policy(s, weight)
+    # now take action
+    action = np.random.choice(len(pol[0]), p=pol[0])
+    # compute gradient
+    grad = s.T @ (np.eye(len(pol[0]))[action] - pol)
+    return action, grad
