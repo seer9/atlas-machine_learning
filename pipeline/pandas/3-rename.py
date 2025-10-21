0cb25e5ad2cfe1f3ac99183b561bd65697130913
clay <rpctropic13@gmail.com>
2025-10-21 08:50:51 -0500

    wasnt able to return df but i was able to print the modified df

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -9,11 +9,6 @@ def rename(df):
         df: The input DataFrame.
     Returns: the modified DataFrame with renamed columns.
     """
-    from_file = __import__('2-from_file').from_file
-
-    # dataset
-    df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
     # Rename timestamp
     df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
 
@@ -21,4 +16,3 @@ def rename(df):
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
 
     print(df[['Datetime', 'Close']].tail())
-    
