c304b8b62f685084a435c39c70cef11c5699830c
clay <rpctropic13@gmail.com>
2025-10-22 20:43:13 -0500

    pycode check

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -15,23 +15,23 @@ if __name__ == "__main__":
     first_launch = sorted_launches[0]
 
     # Extract required information
-    name = first_launch['name']
-    date = first_launch['date']
-    rocket = first_launch['rocket']
-    launchpad = first_launch['launchpad']
+    l_name = first_launch['name']
+    local = first_launch.get('local', 'Unknown Location')
+    rocket_id = first_launch['rocket']
+    launchpad_id = first_launch['launchpad']
 
     # Fetch rocket details
-    rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket}"
+    rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
     rocket_response = requests.get(rocket_url)
     r_name = rocket_response.json()['name']
 
     # Fetch launchpad details
-    launchpad_url = f"https://api.spacexdata.com/v4/launchpads/{launchpad}"
+    launchpad_url = f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}"
     launchpad_response = requests.get(launchpad_url)
     launchpad_data = launchpad_response.json()
     launchpad_name = launchpad_data['name']
-    launchpad_locality = launchpad_data['locality']
+    launchpad_local = launchpad_data['locality']
 
     # Display the formatted result
     print(
-        f"{name} ({date}) {r_name} - {launchpad_name} ({launchpad_locality})")
+        f"{l_name} ({local}) {r_name} - {launchpad_name} ({launchpad_local})")
