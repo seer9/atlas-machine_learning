9f5a37afede8662fbe6498fb3efbe108125e2eec
clay <rpctropic13@gmail.com>
2025-10-29 14:32:59 -0500

    trying a delimiter

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -1,4 +1,5 @@
 -- create a trigger that resets the attribute 'valid_email'
+DELIMITER $$
 CREATE TRIGGER set_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
@@ -6,4 +7,4 @@ BEGIN
     IF OLD.email <> NEW.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END;
+END $$
