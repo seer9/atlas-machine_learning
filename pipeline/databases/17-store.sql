82a3be783a5b0c5ebacacb5f33e9584357c4f5de
clay <rpctropic13@gmail.com>
2025-10-29 14:17:32 -0500

    update inventory

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,8 @@
+-- create a trigger to decrease the quanity of an item column after a new order
+-- DELIMITER $$
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
