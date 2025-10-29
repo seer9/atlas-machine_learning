c0baa00056122cba57b1a454ca47644682a5488f
clay <rpctropic13@gmail.com>
2025-10-29 14:53:03 -0500

    fixing indention

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -14,8 +14,9 @@ BEGIN
     IF project_id IS NULL THEN
         INSERT INTO projects (name)
         VALUES (project_name);
-     END IF;
-        INSERT INTO corrections (user_id, project_id, score) 
-        VALUES (user_id, project_id, score);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) 
+    VALUES (user_id, project_id, score);
 END //
 DELIMITER ;
