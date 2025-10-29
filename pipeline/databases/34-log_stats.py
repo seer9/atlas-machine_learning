0d81f5e2c2585ce819198dbaca7dc87b813af50c
clay <rpctropic13@gmail.com>
2025-10-29 16:09:45 -0500

    new approach for logging

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -9,14 +9,18 @@ if __name__ == '__main__':
     collection = client.logs.nginx
 
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
 
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    m_count = {method: collection.count_documents({"method": method}) for method in methods}
 
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    # Get the count of status check
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
 
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
+
+    # all stats
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in m_count.items():
+        print(f"\tmethod {method}: {count}")
     print(f"{status_check} status check")
