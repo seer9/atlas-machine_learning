9e17cdeca77e360ec5a5184c2bd8e19d6aa3a53d
clay <rpctropic13@gmail.com>
2025-10-29 13:41:40 -0500

    creates a table

---
+++ pipeline/databases/13-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a table with unique users with specific data types
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INT NOT Null AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+    );
