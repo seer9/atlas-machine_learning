cac416498313fde0092be4902f955adf3ee8b59e
clay <rpctropic13@gmail.com>
2025-10-29 14:22:52 -0500

    email validation

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,9 @@
+-- create a trigger that resets the attribute 'valid_email'
+CREATE TRIGGER set_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
