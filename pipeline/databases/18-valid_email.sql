c53027125b1531381890190b90c5ef1dc1b139e3
clay <rpctropic13@gmail.com>
2025-10-29 14:31:16 -0500

    simplifying first approach

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER set_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF STRCMP(old.email, new.email) THEN
+    IF OLD.email <> NEW.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END;
