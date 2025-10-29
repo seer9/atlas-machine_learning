215d1412a8c76a7121aa3e3ec630b714cf540b34
clay <rpctropic13@gmail.com>
2025-10-29 14:02:39 -0500

    glam rocks is the main style

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,7 +1,5 @@
--- ranks country origins of bands, ordered by the number of fans
--- ranks country origins of glam rock bands, ordered by the number of fans
-SELECT origin, SUM(fans) nb_fans
+-- ranks country origins of glam rock bands by lifespan
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
 FROM metal_bands
-WHERE genre = 'Glam Rock'
-GROUP BY origin
-ORDER BY nb_fans DESC;
+WHERE style LIKE '%Glam Rock%'
+ORDER BY lifespan DESC;
