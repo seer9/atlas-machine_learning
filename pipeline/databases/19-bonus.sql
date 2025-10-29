56d7bd031eeb902ccc271f85f249756579eb699b
clay <rpctropic13@gmail.com>
2025-10-29 14:40:16 -0500

    bonus pointss

---
+++ pipeline/databases/19-bonus.sql
@@ -0,0 +1,12 @@
+-- creates a stored procedure AddBonus
+-- adds a new correction for the student
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+    IN student_email VARCHAR(255),
+    IN bonus_points INT
+)
+BEGIN
+    UPDATE students
+    SET points = points + bonus_points
+    WHERE email = student_email;
+END //
