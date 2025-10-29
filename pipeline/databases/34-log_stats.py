f7334dc97755cd96699ac27492a5c6469fcaca69
clay <rpctropic13@gmail.com>
2025-10-29 16:06:33 -0500

    logging nginx stats

---
+++ pipeline/databases/34-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""proivdes stats about nginx logs stored in mongoDB"""
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    client = MongoClient()
+    db = client.logs
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
